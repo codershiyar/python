d9683f7c185439b333dc48f35509d79c1b33f5bd
Coder Shiyar <codershiyar@gmail.com>
2021-07-25 22:40:20 +0200

    Lesson 34 and 35 is added

--- LES34/app.py
+++ LES34/app.py
@@ -2,21 +2,17 @@ import json
 # json.dumps()        يمكنك استخدامها لتحويل بيانات من نوع اوبجكت ديكجوناري إلى بيانات بصيغة جي سون
 # json.loads() يتم استخدامها لتحويل البيانات مكتوبة بصيغة جي سون إلى بيانات بصيغة اوبجكت ديكجوناري
 
+data = {
+        "name":"Shiyar",
+        "age": 21 ,
+         "isMarried":False
+        }
 
-data= '{"name": "Shiyar",  "age" : 21 ,  "isMaried" : false }'
-data_dict = json.loads(data)
-print(data_dict["name"]   )         
-print( type(json.loads(data))  )  
+data_json = '{"name": "Shiyar", "age": 21, "isMarried": false}'
+data_json = json.loads(data_json) 
+print(data_json["name"])
+# print(type( json.loads(data_json)) )
 
-
-# data= {
-#      "name": "Shiyar",
-#       "age" : 21 ,
-#       "isMaried" : False
-#     }
-
-# print( json.dumps(data) )         
-# print( type(json.dumps(data))  )  
 
 
 # If you have a JSON string, you can parse it by using the json.loads() method.
---
+++ LES35/app.py
@@ -0,0 +1,13 @@
+class App:
+    name = "Coder Shiyar"
+    version = 10
+    text 
+    def __init__(self,course,text):
+       self.course = course
+       self.text = text
+
+app = App("Python","انا احب بايثون")
+
+
+print(app.name)
+
